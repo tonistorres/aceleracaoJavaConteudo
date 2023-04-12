a92c6d3f15a4ae745ebfd45df980b8d1033d85da
Tonis Torres <sisvenda2011@gmail.com>
2023-04-12 10:41:17 -0300

    feat: adding class Float.java in github

---
+++ VariaveisTipos/src/br/com/i9/conhecimento/tipofloat/Float.java
@@ -0,0 +1,16 @@
+package br.com.i9.conhecimento.tipofloat;
+
+/**
+ * O float é um tipo primitivo numérico de range -1,4024E-37 até 3,40282347E+38.
+ * Ele é um dos tipos primitivos usados para trabalhar com números de ponto
+ * flutuante, servindo para garantir precisão nos cálculos e para notação
+ * científica. Bora descobrir um pouco mais?                */
+public class Float {
+
+	public static void main(String[] args) {
+		float numA = -101.23f;
+		float numB = 2.356f;
+		System.out.println(numA + numB); // -98.874
+	}
+
+}
